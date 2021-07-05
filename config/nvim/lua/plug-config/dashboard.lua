e1ce00649c249f8843fac7be8303b96473881007
Danny Loor <danny_loor00@hotmail.com>
2021-07-04 20:45:41 -0500

    Git plugins config

--- config/nvim/lua/plug-config/dashboard.lua
+++ config/nvim/lua/plug-config/dashboard.lua
@@ -14,12 +14,12 @@ vim.g.dashboard_custom_header = {
 -- Default exec
 vim.g.dashboard_default_executive ='telescope'
 
--- Hide Tabline
-vim.cmd([[
-set showtabline=0 | autocmd WinLeave <buffer> set showtabline=2
-]])
+-- -- Hide Tabline
+-- vim.cmd([[
+-- set showtabline=0 | autocmd WinLeave <buffer> set showtabline=2
+-- ]])
 
--- Hide StatusLine
-vim.cmd([[
-set laststatus=0 | autocmd WinLeave <buffer> set laststatus=2
-]])
+-- -- Hide StatusLine
+-- vim.cmd([[
+-- set laststatus=0 | autocmd WinLeave <buffer> set laststatus=2
+-- ]])
